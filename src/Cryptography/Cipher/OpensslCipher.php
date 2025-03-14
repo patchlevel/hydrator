bf972297ae9ff6885a32eb7e5d3927797f3d49bc
David Badura <david.badura@patchlevel.de>
2025-03-14 13:38:40 +0100

    add crypto prefix

--- src/Cryptography/Cipher/OpensslCipher.php
+++ src/Cryptography/Cipher/OpensslCipher.php
@@ -17,8 +17,17 @@ use const JSON_THROW_ON_ERROR;
 
 final class OpensslCipher implements Cipher
 {
+    public function __construct(
+        private readonly string $prefix = '',
+    ) {
+    }
+
     public function encrypt(CipherKey $key, mixed $data): string
     {
+        if ($this->prefix && is_string($data) && str_starts_with($data, $this->prefix)) {
+            throw new EncryptionFailed();
+        }
+
         $encryptedData = @openssl_encrypt(
             $this->dataEncode($data),
             $key->method,
@@ -31,11 +40,19 @@ final class OpensslCipher implements Cipher
             throw new EncryptionFailed();
         }
 
-        return base64_encode($encryptedData);
+        return $this->prefix . base64_encode($encryptedData);
     }
 
     public function decrypt(CipherKey $key, string $data): mixed
     {
+        if ($this->prefix) {
+            if (str_starts_with($data, $this->prefix)) {
+                $data = substr($data, strlen($this->prefix));
+            } else {
+                return $data;
+            }
+        }
+
         $data = @openssl_decrypt(
             base64_decode($data),
             $key->method,
