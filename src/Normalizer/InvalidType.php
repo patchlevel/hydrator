be9169f04757299c7518e9dd611fa6ece499e49a
David Badura <david.badura@patchlevel.de>
2025-04-19 09:59:17 +0200

    fix tests

--- src/Normalizer/InvalidType.php
+++ src/Normalizer/InvalidType.php
@@ -6,12 +6,13 @@ namespace Patchlevel\Hydrator\Normalizer;
 
 use InvalidArgumentException;
 use Patchlevel\Hydrator\HydratorException;
+use Symfony\Component\TypeInfo\Type;
 
 use function sprintf;
 
 final class InvalidType extends InvalidArgumentException implements HydratorException
 {
-    public static function unsupportedType(string $expectedType, string $name): self
+    public static function unsupportedType(Type $expectedType, Type $name): self
     {
         return new self(sprintf('Unsupported type "%s", expected "%s".', $name, $expectedType));
     }
